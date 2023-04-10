6327c3b23e2c17403a32c967b6dc7249dbfa2faf
Dr-ZHUIM <372919916@qq.com>
2023-04-10 22:15:20 +0800

    list

---
+++ src/com/zhuim/list/list.kt
@@ -0,0 +1,19 @@
+package com.zhuim.list
+
+fun main (){
+    val list = listOf("first","second")
+    val firstElement = list.getOrElse(0){"error: there is no such element!"}
+    val secondElement = list.getOrElse(1){"error: there is no such element!"}
+    val thirdElementButError = list.getOrElse(2){"error: there is no such element!"}
+    val fourthElementButNull = list.getOrNull(3)
+    println("$firstElement $secondElement $thirdElementButError ${fourthElementButNull ?: "oh yh the forth element is Null!"}")
+
+    val mutableList = list.toMutableList()
+    mutableList.add("bb");
+    val thirdElementButError2 = list.getOrElse(2){"error: there is no such element!"}
+    val thirdElement2 = mutableList.getOrElse(2){"error: there is no such element!"}
+    println("$thirdElementButError2")
+    println("$thirdElement2")
+
+    val immutableList = mutableList.toList()
+}
